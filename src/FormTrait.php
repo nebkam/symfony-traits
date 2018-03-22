12e0b7975dec25ce844d890f9e04dd9ddcdaa0b5
Nebojša Kamber <kamber@nebojsa.in.rs>
2018-03-22 14:34:05 +0100

    FormTrait::handleForm

--- src/FormTrait.php
+++ src/FormTrait.php
@@ -15,6 +15,21 @@ trait FormTrait
 	/**
 	 * If this method does not throw an exception, consider the form valid
 	 *
+	 * @param Request $request
+	 * @param mixed $domain
+	 * @param string $formType
+	 */
+	protected function handleForm(Request $request, $domain, $formType)
+		{
+		$form = $this->createForm($formType,$domain);
+		$form->handleRequest($request);
+
+		$this->handleFormErrors($form);
+		}
+
+	/**
+	 * If this method does not throw an exception, consider the form valid
+	 *
 	 * @param Request $request Request with JSON posted data
 	 * @param object $domain
 	 * @param string $formType
